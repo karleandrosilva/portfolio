121a3a6a66e01a209ab56b8e69dfa925646118fe
karleandrosilva <contatoparakarle@gmail.com>
2026-04-04 15:26:40 -0300

    feat: adiciona novo componente header

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MenuDrawer } from "../../components/menu-drawer/menu-drawer";
+import { Header } from '../../components/header/header';
 
 @Component({
   selector: 'app-home',
-  imports: [MenuDrawer],
+  imports: [MenuDrawer, Header],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
