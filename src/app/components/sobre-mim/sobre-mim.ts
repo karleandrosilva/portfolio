9254c9036f58852990edfb35d9e18c60be41ec5b
karleandrosilva <contatoparakarle@gmail.com>
2026-04-05 23:56:22 -0300

    feat: adiciona componente sobre-mim

---
+++ src/app/components/sobre-mim/sobre-mim.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from "@angular/common";
+
+@Component({
+  selector: 'app-sobre-mim',
+  imports: [NgOptimizedImage],
+  templateUrl: './sobre-mim.html',
+  styleUrl: './sobre-mim.scss',
+})
+export class SobreMim {
+
+}
