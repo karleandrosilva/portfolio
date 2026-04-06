555d1e697194620f5f4fc6da28f70f2123988842
karleandrosilva <contatoparakarle@gmail.com>
2026-04-06 00:17:41 -0300

    fix: corrige dimensoes das imagens

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuDrawer } from "../../components/menu-drawer/menu-drawer";
 import { Header } from '../../components/header/header';
+import { SobreMim } from "../../components/sobre-mim/sobre-mim";
 
 @Component({
   selector: 'app-home',
-  imports: [MenuDrawer, Header],
+  imports: [MenuDrawer, Header, SobreMim],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
